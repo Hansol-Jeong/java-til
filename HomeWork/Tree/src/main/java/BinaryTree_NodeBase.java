7123d950a2bbf4c983f796f581318029719abb5e
Hansol-Jeong <shn03014@naver.com>
2021-01-13 06:24:48 +0900

    .

--- HomeWork/Tree/src/main/java/BinaryTree_NodeBase.java
+++ HomeWork/Tree/src/main/java/BinaryTree_NodeBase.java
@@ -19,7 +19,7 @@ public class BinaryTree_NodeBase implements Tree{
     private boolean isFound;
     public Node[] nodeArray;
 
-    public BinaryTree_NodeBase(int [] array) {//초기화 박는 과정 배열을 받으면 그걸 index 기준으로 binaryTree로 변환
+    public BinaryTree_NodeBase(int [] array) {
         nodeArray = init(array);
     }
 
@@ -28,15 +28,13 @@ public class BinaryTree_NodeBase implements Tree{
         preorderHelper(root);
         System.out.println();
     }
-    public void preorderHelper(Node root) {//나중에 root노드 지정해 주면 되겠지
-        //노드 -> 왼족 -> 오른쪽
+    public void preorderHelper(Node root) {
         System.out.print(root.getValue() + " ");
 
         //리컬시브하게!!
-        if(root.getLeft() != null) {//left값이 있어요! 그러면 left로 가서노드값을 찍어줘야 겠죠?
-//            currNode = currNode.getLeft();//왼쪽 왔다 리컬시브하게하라고
+        if(root.getLeft() != null) {
             preorderHelper(root.getLeft());
-        } //계속 왼쪾으로 가다가 왼쪽 값 없으면 메소드콜한 이 지점으로 다시 오겠지
+        }
 
         if(root.getRight() != null) {
             preorderHelper(root.getRight());
@@ -51,7 +49,6 @@ public class BinaryTree_NodeBase implements Tree{
 
     public void inorderHelper(Node root) {
 
-        //리컬시브하게!!
         if(root.getLeft() != null) {
             inorderHelper(root.getLeft());
         }
@@ -71,10 +68,6 @@ public class BinaryTree_NodeBase implements Tree{
     }
 
     public void postorderHelper(Node root) {
-//        Node currNode = root;
-
-
-        //리컬시브하게!!
         if(root.getLeft() != null) {
             inorderHelper(root.getLeft());
         }
@@ -105,24 +98,23 @@ public class BinaryTree_NodeBase implements Tree{
 
     public void dfsHelper(Node root, int value) {
         if(isFound)
-            return; //메소드의 종료를 위해 리컬시브 다 종료시킴
+            return;
 
         if(root.getValue() == value) {
             isFound = true;
-            return; //찾으면 끝내고
+            return;
         }
-        //못찾으면
         if(root.getLeft() != null) {
-            dfsHelper(root.getLeft(), value);//왼쪽가서 값이랑 맞는지 다시 비교해봐
+            dfsHelper(root.getLeft(), value);
         }
         if(root.getRight() != null) {
-            dfsHelper(root.getRight(), value);//왼쪽가서 값이랑 맞는지 다시 비교해봐
+            dfsHelper(root.getRight(), value);
         }
     }
 
     public Node[] init(int [] array) {
 
-        Node [] nodeArray = new Node[array.length];// 해당 배열의 길이만큼 노드 배열을 만들고
+        Node [] nodeArray = new Node[array.length];
         for(int i = 0; i < nodeArray.length; i++) {// 이렇게 나눠서 하면돼 -> 예상: nodeArray[]배열 값 내부가 null이어서 참조를 못했나?
             nodeArray[i] = new Node(array[i], null, null);
         }
@@ -136,7 +128,7 @@ public class BinaryTree_NodeBase implements Tree{
                 nodeArray[i].setRight(nodeArray[right]);
             }
         }
- /*       for (int i = 0; i < nodeArray.length; i++) { //left right 설정이 안되잖아? 왜ㅏㅇ노디느대럼ㄷ러ㅣㅁㅈ덜
+ /*       for (int i = 0; i < nodeArray.length; i++) { //left right 설정이 안되잖아? 왜ㅏㅇ노디느대럼ㄷ러ㅣㅁㅈ덜 -> 질문
             nodeArray[i] = new Node(array[i], null, null);
            *//* nodeArray[i] = new Node.NodeBuilder()
                     .value(array[i])
